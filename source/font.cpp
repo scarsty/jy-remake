8a28239fee2577cc10f87c0476a87d382318ae6f
Zgames <zgames@yeah.net>
2015-01-12 14:21:09 +0800

    存档

--- source/font.cpp
+++ source/font.cpp
@@ -1,10 +1,12 @@
 /*
- * GB2312编码, 16 x 16 点阵汉字支持。
- *
- *
- */
+==========================================================================================
+font.cpp - 内置的点阵字库
+Note: 支持GB2312编码的汉字和ASCII编码的英文字符。
+==========================================================================================
+*/
 
-#include <SDL.h>
+#include <SDL_video.h>
+#include <SDL_stdinc.h>
 #include <string.h>
 
 static Uint8 fontData[] =
@@ -14,35 +16,6 @@ static Uint8 fontData[] =
 static Uint8 asciiData[] = 
 #include "asciidata.cpp"
 ;
-
-// 32 - 127
-static void DrawAsciiChar2(SDL_Renderer *renderer, const Uint8 *ptr, int x, int y)
-{
-    int i, j, k, offset;
-    int flag;
-    //unsigned char buffer[32];
-    Uint8 *buffer = NULL;
-    Uint8 key[8] = {
-        1<<7, 1<<6, 1<<5, 1<<4, 1<<3, 1<<2, 1<<1, 1
-    };
-    SDL_Point points[128]; // 每个字模最多128个点。
-    int numPts = 0;
-
-    offset = (*ptr - 32) * 16;
-    buffer = &asciiData[offset];
-
-    for(k=0; k<16; k++) {
-        for(i=0; i<8; i++){
-            flag = buffer[k] & key[i];
-            if (flag) {
-                points[numPts].x = i + x;
-                points[numPts].y = k + y;
-                numPts++;
-            }
-        }
-    }
-    SDL_RenderDrawPoints(renderer, points, numPts);
-}
 
 static Uint32 drawColor = 0;
 
@@ -68,8 +41,6 @@ static void DrawAsciiChar(SDL_Surface *surface, const Uint8 *ptr, int x, int y)
     Uint8 key[8] = {
         1<<7, 1<<6, 1<<5, 1<<4, 1<<3, 1<<2, 1<<1, 1
     };
-    SDL_Point points[128]; // 每个字模最多128个点。
-    int numPts = 0;
 
     offset = (*ptr - 32) * 16;
     buffer = &asciiData[offset];
@@ -82,39 +53,6 @@ static void DrawAsciiChar(SDL_Surface *surface, const Uint8 *ptr, int x, int y)
             }
         }
     }
-}
-
-static void DrawGbkChar2(SDL_Renderer *renderer, const Uint8 *word, int x, int y)
-{
-    int i, j, k, offset;
-    int flag;
-    //unsigned char buffer[32];
-    unsigned char *buffer = NULL;
-    //unsigned char word[3] = "Ａ";
-    unsigned char key[8] = {
-        1<<7, 1<<6, 1<<5, 1<<4, 1<<3, 1<<2, 1<<1, 1
-    };
-    SDL_Point points[256];
-    int numPts;
-
-    //offset = (94*(unsigned int)(word[0]-0xa0-1)+(word[1]-0xa0-1))*32;
-    offset = (94*(Uint32)(word[0]-0xa0-1)+(word[1]-0xa0-1))*32;
-    buffer = &fontData[offset];
-
-    for(k=0; k<16; k++) {
-        for(j=0; j<2; j++){
-            for(i=0; i<8; i++){
-                flag = buffer[k*2+j] & key[i];
-                if (flag) {
-                    points[numPts].x = j*8 + i + x;
-                    points[numPts].y = k + y;
-                    numPts++;
-                }
-            }
-        }
-    }
-
-    SDL_RenderDrawPoints(renderer, points, numPts);
 }
 
 static void DrawGbkChar(SDL_Surface *surface, const Uint8 *word, int x, int y)
@@ -140,23 +78,6 @@ static void DrawGbkChar(SDL_Surface *surface, const Uint8 *word, int x, int y)
                     putpixel32(surface, j*8 + i + x, k + y);
                 }
             }
-        }
-    }
-}
-
-
-void Font_DrawText2(SDL_Renderer *renderer, const char *str, int x, int y)
-{
-    const unsigned char *p = (const unsigned char *) str;
-    while (*p) {
-        if (*p<128) {
-            DrawAsciiChar2(renderer, p, x, y);
-            p++;
-            x += 8;
-        } else {
-            DrawGbkChar2(renderer, p, x, y);
-            p+=2;
-            x += 16;
         }
     }
 }
